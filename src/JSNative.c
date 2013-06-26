feb7258b9d3e13e6d3d68fdba9c476fd577d60b5
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-06-26 15:21:43 -0500

    "Exceptional Coverage"
    operational exception additions
    
    Modified: src/JSNative.c

--- src/JSNative.c
+++ src/JSNative.c
@@ -40,6 +40,20 @@ static void js_native_throw_exception(JSContextRef ctx, char * message, JSValueR
 	js_native_eval_scriptlet(ctx, output, NULL, exception);
 }
 
+static bool js_native_jsstring_parse_int(JSContextRef ctx, JSStringRef s, unsigned long int * i, JSValueRef * exception) {
+	unsigned long int count = JSStringGetMaximumUTF8CStringSize(s);
+	char copy[count], output[count + 19];
+	JSStringGetUTF8CString (s, copy, count);
+	snprintf(output, count + 19, "isNaN(parseInt(\"%s\"));", copy);
+	if ( ! JSValueToBoolean(ctx, js_native_eval_scriptlet(ctx, output, NULL, exception))) {
+		snprintf(output, count + 11, "parseInt(\"%s\");", copy);
+		*i = (unsigned long int) JSValueToNumber(ctx, js_native_eval_scriptlet(ctx, output, NULL, exception), exception);		
+	} else {
+		return false;
+	}
+	return true;
+}
+
 static JSValueRef js_native_call_function(JSContextRef ctx, char * function, JSObjectRef object, int argc, JSValueRef arguments[], JSValueRef * exception) {
 	JSObjectRef call = (void*) js_native_eval_scriptlet(ctx, function, object, exception); // check exception!
 	return JSObjectCallAsFunction(ctx, call, object, argc, arguments, exception); // forward exception
@@ -74,16 +88,30 @@ static JSObjectRef js_native_construct_object (JSContextRef ctx, JSObjectRef con
 	return JSObjectMake(ctx, JSNative, p);
 }
 
-JSValueRef js_native_get_property_at_index(JSContextRef ctx, JSNativePrivate * p, unsigned long index) {
-	if ( ! p || ! p->location ) return NULL;
-	if (index && index >= p->count) return NULL;
-	if (p->typeCode == JSNativeTypeVoid) return JSValueMakeNull(ctx);
+JSValueRef js_native_get_property_at_index(JSContextRef ctx, JSNativePrivate * p, unsigned long index, JSValueRef * exception) {
+
+	char * e[] = { // error strings
+		"js_native_get_property_at_index: no native data available",
+		"js_native_get_property_at_index: cannot read null base pointer",
+		"js_native_get_property_at_index: cannot read range overflow",
+		"js_native_get_property_at_index: cannot read void type",
+		"js_native_get_property_at_index: cannot read unknown native type code",
+		NULL
+	};
+
+	if ( ! p ) { js_native_throw_exception(ctx, e[0], exception); return NULL; }
+	if ( ! p->location ) { js_native_throw_exception(ctx, e[1], exception); JSValueMakeNull(ctx); }
+	if (index && index >= p->count || p->count == 0) { js_native_throw_exception(ctx, e[2], exception); JSValueMakeNull(ctx); }
+
+	if (p->typeCode == JSNativeTypeVoid) { js_native_throw_exception(ctx, e[3], exception); JSValueMakeNull(ctx); }
+
 	if (JSNativeUnsignedValue(p->typeCode)) {
 		if (p->typeCode == JSNativeTypeChar + 1) return JSValueMakeNumber(ctx, (double) (unsigned char) *(unsigned char*)(p->location + index));
 		else if (p->typeCode == JSNativeTypeShort + 1) return JSValueMakeNumber(ctx, (double) *(unsigned short*)(p->location + index));
 		else if (p->typeCode == JSNativeTypeInt + 1) return JSValueMakeNumber(ctx, (double) *(unsigned int*)(p->location + index));
 		else if (p->typeCode == JSNativeTypeLong + 1) return JSValueMakeNumber(ctx, (double) *(unsigned long*)(p->location + index));
 		else if (p->typeCode == JSNativeTypeLongLong + 1) return JSValueMakeNumber(ctx, (double) *(unsigned long long*)(p->location + index));
+		else { js_native_throw_exception(ctx, e[4], exception); return JSValueMakeNull(ctx); }
 	} else {
 		if (p->typeCode == JSNativeTypeBool) return JSValueMakeNumber(ctx, (double) *(bool*)(p->location + index));
 		else if (p->typeCode == JSNativeTypeChar) return JSValueMakeNumber(ctx, (double) *(char*)(p->location + index));
@@ -94,120 +122,111 @@ JSValueRef js_native_get_property_at_index(JSContextRef ctx, JSNativePrivate * p
 		else if (p->typeCode == JSNativeTypeFloat) return JSValueMakeNumber(ctx, (double) *(float*)(p->location + index));
 		else if (p->typeCode == JSNativeTypeDouble) return JSValueMakeNumber(ctx, (double) *(double*)(p->location + index));
 		else if (p->typeCode == JSNativeTypePointer) return JSValueMakeNumber(ctx, (double) *(unsigned long*)(p->location + index));
+		else { js_native_throw_exception(ctx, e[4], exception); return JSValueMakeNull(ctx); }
 	}
+
 	return NULL;
+
 }
 
 static JSValueRef js_native_get_property (JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef* exception) {
-	JSNativePrivate * p = JSObjectGetPrivate(object);
-	if (!p) return NULL;
-	if (!p->location) return NULL;
+
+	JSNativePrivate * p = JSObjectGetPrivate(object); if (!p) return NULL;
+	unsigned long index = 0;
 	if (JSStringIsEqualToUTF8CString(propertyName, "pointer")) {
 		return JSValueMakeNumber(ctx, (double) (unsigned long) p->location);
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "value")) {
-		return js_native_get_property_at_index(ctx, p, 0);
+		return js_native_get_property_at_index(ctx, p, 0, exception);
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "type")) {
 		return JSValueMakeNumber(ctx, (double) p->typeCode);
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "count")) {
 		return JSValueMakeNumber(ctx, (double) (unsigned long) p->count);
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "constant")) {
 		return JSValueMakeBoolean(ctx, p->constant);
+	} else if (js_native_jsstring_parse_int(ctx, propertyName, &index, exception)) {
+		return js_native_get_property_at_index(ctx, p, index, exception);
 	}
-	unsigned long length = JSStringGetMaximumUTF8CStringSize (propertyName);
-	char * buf = g_malloc (length * sizeof (gchar));
-	JSStringGetUTF8CString (propertyName, buf, length);
-	if (! isdigit(*buf) ) {
-		g_free(buf); return NULL;
-	} else {
-		unsigned long index = atol(buf); g_free(buf);
-		return js_native_get_property_at_index(ctx, p, index);
-	}
+	return NULL;
 }
 
-bool js_native_set_property_at_index(JSContextRef ctx, JSNativePrivate * p, unsigned long index, JSValueRef value) {
-	if (! p || ! p->location || ! p->count) return false;
-	if (index && index >= p->count) return false;
-	if ( p->constant) return true;
+bool js_native_set_property_at_index(JSContextRef ctx, JSNativePrivate * p, unsigned long index, JSValueRef value, JSValueRef * exception) {
+
+	char * e[] = { // error strings
+		"js_native_set_property_at_index: no native data available",
+		"js_native_set_property_at_index: cannot write null base pointer",
+		"js_native_set_property_at_index: cannot write range overflow",
+		"js_native_set_property_at_index: cannot write void type",
+		"js_native_set_property_at_index: cannot write constant value",
+		"js_native_set_property_at_index: cannot write unknown native type code",
+		NULL
+	};
+
+	if ( ! p ) { js_native_throw_exception(ctx, e[0], exception); return false; }
+	if ( ! p->location ) { js_native_throw_exception(ctx, e[1], exception); return true; }
+	if (index && index >= p->count || p->count == 0) { js_native_throw_exception(ctx, e[2], exception); return true; }
+	if (p->typeCode == JSNativeTypeVoid) { js_native_throw_exception(ctx, e[3], exception); return true; }
+	if ( p->constant) { js_native_throw_exception(ctx, e[4], exception); return true; }
+
 	if (JSNativeUnsignedValue(p->typeCode)) {
-		if (p->typeCode == JSNativeTypeChar + 1) { *(unsigned char*)(p->location + index) = (unsigned char) JSValueToNumber(ctx, value, NULL); return true; }
-		else if (p->typeCode == JSNativeTypeShort + 1) { *(unsigned short*)(p->location + index) = (unsigned short) JSValueToNumber(ctx, value, NULL); return true; }
-		else if (p->typeCode == JSNativeTypeInt + 1) { *(unsigned int*)(p->location + index) = (unsigned int) JSValueToNumber(ctx, value, NULL); return true; }
-		else if (p->typeCode == JSNativeTypeLong + 1) { *(unsigned long*)(p->location + index) = (unsigned long) JSValueToNumber(ctx, value, NULL); return true; }
-		else if (p->typeCode == JSNativeTypeLongLong + 1) { *(unsigned long long*)(p->location + index) = (unsigned long long) JSValueToNumber(ctx, value, NULL); return true; }
+		if (p->typeCode == JSNativeTypeChar + 1) { *(unsigned char*)(p->location + index) = (unsigned char) JSValueToNumber(ctx, value, exception); return true; }
+		else if (p->typeCode == JSNativeTypeShort + 1) { *(unsigned short*)(p->location + index) = (unsigned short) JSValueToNumber(ctx, value, exception); return true; }
+		else if (p->typeCode == JSNativeTypeInt + 1) { *(unsigned int*)(p->location + index) = (unsigned int) JSValueToNumber(ctx, value, exception); return true; }
+		else if (p->typeCode == JSNativeTypeLong + 1) { *(unsigned long*)(p->location + index) = (unsigned long) JSValueToNumber(ctx, value, exception); return true; }
+		else if (p->typeCode == JSNativeTypeLongLong + 1) { *(unsigned long long*)(p->location + index) = (unsigned long long) JSValueToNumber(ctx, value, exception); return true; }
+		else { js_native_throw_exception(ctx, e[5], exception); return true; }
 	} else {
 		if (p->typeCode == JSNativeTypeBool) { *(bool*)(p->location + index) = JSValueToBoolean(ctx, value); return true; }
-		else if (p->typeCode == JSNativeTypeChar) { *(char*)(p->location + index) = (char) JSValueToNumber(ctx, value, NULL); return true; }
-		else if (p->typeCode == JSNativeTypeShort) { *(short*)(p->location + index) = (short) JSValueToNumber(ctx, value, NULL); return true; }
-		else if (p->typeCode == JSNativeTypeInt) { *(int*)(p->location + index) = (int) JSValueToNumber(ctx, value, NULL); return true; }
-		else if (p->typeCode == JSNativeTypeLong) { *(long*)(p->location + index) = (long) JSValueToNumber(ctx, value, NULL); return true; } 
-		else if (p->typeCode == JSNativeTypeLongLong) { *(long long*)(p->location + index) = (long long) JSValueToNumber(ctx, value, NULL); return true; } 
-		else if (p->typeCode == JSNativeTypeFloat) { *(float*)(p->location + index) = (float) JSValueToNumber(ctx, value, NULL); return true; } 
-		else if (p->typeCode == JSNativeTypeDouble) { *(double*)(p->location + index) = JSValueToNumber(ctx, value, NULL); return true; } 
-		else if (p->typeCode == JSNativeTypePointer) { *(unsigned long*)(p->location + index) = (unsigned long) JSValueToNumber(ctx, value, NULL); return true; }
+		else if (p->typeCode == JSNativeTypeChar) { *(char*)(p->location + index) = (char) JSValueToNumber(ctx, value, exception); return true; }
+		else if (p->typeCode == JSNativeTypeShort) { *(short*)(p->location + index) = (short) JSValueToNumber(ctx, value, exception); return true; }
+		else if (p->typeCode == JSNativeTypeInt) { *(int*)(p->location + index) = (int) JSValueToNumber(ctx, value, exception); return true; }
+		else if (p->typeCode == JSNativeTypeLong) { *(long*)(p->location + index) = (long) JSValueToNumber(ctx, value, exception); return true; } 
+		else if (p->typeCode == JSNativeTypeLongLong) { *(long long*)(p->location + index) = (long long) JSValueToNumber(ctx, value, exception); return true; } 
+		else if (p->typeCode == JSNativeTypeFloat) { *(float*)(p->location + index) = (float) JSValueToNumber(ctx, value, exception); return true; } 
+		else if (p->typeCode == JSNativeTypeDouble) { *(double*)(p->location + index) = JSValueToNumber(ctx, value, exception); return true; } 
+		else if (p->typeCode == JSNativeTypePointer) { *(unsigned long*)(p->location + index) = (unsigned long) JSValueToNumber(ctx, value, exception); return true; }
+		else { js_native_throw_exception(ctx, e[5], exception); return true; }
 	}
+
 	return false;
 }
 
 static bool js_native_set_property (JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, JSValueRef* exception) {
-
 	JSNativePrivate * p = JSObjectGetPrivate(object);
-	if (!p) { return false; }
-
+	unsigned long index = 0;
 	char * e[] = { // error strings
 		"js_native_set_property: pointer is allocated, read only",
-		"js_native_set_property: unable to set value, pointer property not set",
 		"js_native_set_property: pointer is allocated, count is read only",
-		"js_native_set_property: destination index exceeds defined access range",
 		NULL
 	};
-
+	if (!p) { return false; }
 	if (JSStringIsEqualToUTF8CString(propertyName, "pointer")) {
 		if (p->allocated) { js_native_throw_exception(ctx, e[0], exception); return true; }
-		p->location = (void *) (unsigned long) JSValueToNumber(ctx, value, exception);
-		return true;
+		p->location = (void *) (unsigned long) JSValueToNumber(ctx, value, exception); return true;
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "value")) {
-		if (p->location == NULL || p->count == 0) { js_native_throw_exception(ctx, e[1], exception); return true; }
-		return js_native_set_property_at_index(ctx, p, 0, value);
+		return js_native_set_property_at_index(ctx, p, 0, value, exception);
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "type")) {
-		p->typeCode = (unsigned int) JSValueToNumber(ctx, value, exception);
-		return true;
+		p->typeCode = (unsigned int) JSValueToNumber(ctx, value, exception); return true;
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "count")) {
-		if (p->allocated) {
-			js_native_throw_exception(ctx, e[2], exception);
-			return true;
-		}
-		p->count = (unsigned long) JSValueToNumber(ctx, value, exception);
-		return true;
+		if (p->allocated) { js_native_throw_exception(ctx, e[1], exception); return true; }
+		p->count = (unsigned long) JSValueToNumber(ctx, value, exception); return true;
 	} else if (JSStringIsEqualToUTF8CString(propertyName, "constant")) {
-		p->constant = JSValueToBoolean(ctx, value);
-		return true;
+		p->constant = JSValueToBoolean(ctx, value); return true;
+	} else if (js_native_jsstring_parse_int(ctx, propertyName, &index, exception)) {
+		return js_native_set_property_at_index(ctx, p, index, value, exception);
 	}
-
-	if (p->location == NULL || p->count == 0) { js_native_throw_exception(ctx, e[1], exception); return true; }
-
-	unsigned long length = JSStringGetMaximumUTF8CStringSize (propertyName);
-	void * buf = g_malloc (length * sizeof (gchar));
-	JSStringGetUTF8CString (propertyName, buf, length);
-	unsigned long index = atol(buf); g_free(buf);
-
-	if (index && index >= p->count) {
-		js_native_throw_exception(ctx, e[3], exception); return true;
-	}
-
-	return js_native_set_property_at_index(ctx, p, index, value);
-
+	return false;
 }
 
 // The callback invoked when determining whether an object has a property.
 static bool js_native_has_property(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName) {
 	JSNativePrivate * p = JSObjectGetPrivate(object);
 	if (!p) return false;
-	if (JSStringIsEqualToUTF8CString(propertyName, "pointer")) return true;
-	if (JSStringIsEqualToUTF8CString(propertyName, "value")) return true;
-	if (JSStringIsEqualToUTF8CString(propertyName, "type")) return true;
-	if (JSStringIsEqualToUTF8CString(propertyName, "count")) return true;
-	if (JSStringIsEqualToUTF8CString(propertyName, "constant")) return true;
-	return false;
+	else if (JSStringIsEqualToUTF8CString(propertyName, "pointer")) return true;
+	else if (JSStringIsEqualToUTF8CString(propertyName, "value")) return true;
+	else if (JSStringIsEqualToUTF8CString(propertyName, "type")) return true;
+	else if (JSStringIsEqualToUTF8CString(propertyName, "count")) return true;
+	else if (JSStringIsEqualToUTF8CString(propertyName, "constant")) return true;
+	else return false;
 }
 
 JSValueRef js_native_free(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception) {
@@ -225,6 +244,7 @@ JSClassRef js_native_define_class(JSContextRef ctx, JSObjectRef object) {
 	jsNative.callAsConstructor = &js_native_construct_object;
 	jsNative.setProperty = &js_native_set_property;
 	jsNative.getProperty = &js_native_get_property;
+	jsNative.hasProperty = &js_native_has_property;
 	JSStaticFunction StaticFunctionArray[] = {
 	    { "free", &js_native_free, kJSPropertyAttributeDontDelete  }, { 0, 0, 0 }
 	};
