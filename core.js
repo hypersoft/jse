99664dc96d32f0b12fcb9996e7a1eec7c814522a
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-06-25 20:11:21 -0500

    Show and tell
    Some basic concept walkthroughs.

--- core.js
+++ core.js
@@ -2,6 +2,20 @@
 
 try {
 
-JSNative.defineAlias("long long", "int64");
-puts("unsigned int64: " + JSNative.typeCode("unsigned int64"));
+JSNative.defineAlias("unsigned char", "byte");
+puts("byte type code: " + JSNative.typeCode("byte"));
+puts("byte type code to prototype: " + JSNative.typeCodeToPrototype(JSNative.typeCode("byte")));
+
+// Each native must be constructed.
+byteArray = new JSNative("byte", 32);
+puts("byte array type: " + byteArray + " " + JSNative.typeCodeToPrototype(byteArray.type));
+puts("byte array count: " + byteArray.count);
+puts("byte array pointer: " + byteArray.pointer);
+
+byteArray[0] = 64; // zero is the default value
+puts("byte array default value: " + byteArray.value);
+
+// And freed.
+byteArray.free();
+
 } catch (e) { puts(e); };
