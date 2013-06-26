d2ffab01eb1e5236026466b2bd4d008ae1a3274a
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-06-26 10:00:59 -0500

    "Rock 'em sock 'em robot"
    Modified: core.js
    Modified: src/JSNative.c

--- core.js
+++ core.js
@@ -2,20 +2,32 @@
 
 try {
 
-JSNative.defineAlias("unsigned char", "byte");
-puts("byte type code: " + JSNative.typeCode("byte"));
-puts("byte type code to prototype: " + JSNative.typeCodeToPrototype(JSNative.typeCode("byte")));
+// Mockup runtime c interpreter for operational clarity...
+puts("typedef unsigned char byte;");
+	JSNative.defineAlias("unsigned char", "byte");
+	puts(JSNative.typeCodeToPrototype(JSNative.typeCode("byte")) + " (" + JSNative.typeCode("byte") + ")");
 
 // Each native must be constructed.
-byteArray = new JSNative("byte", 32);
-puts("byte array type: " + byteArray + " " + JSNative.typeCodeToPrototype(byteArray.type));
-puts("byte array count: " + byteArray.count);
-puts("byte array pointer: " + byteArray.pointer);
+puts("byte byteArray[32];");
+	byteArray = new JSNative("byte", 32);
+	puts(byteArray.pointer);
 
-byteArray[0] = 64; // zero is the default value
-puts("byte array default value: " + byteArray.value);
+puts("byteArray[0] = 64;");
+	byteArray[0] = 64; // zero is the default value
+	puts(byteArray[0]);
 
-// And freed.
-byteArray.free();
+// The void type provides a fully customizable JSNative without an allocated data backing
+puts("void * pVoid = (void *) byteArray;");
+	pVoid = new JSNative("void");
+	pVoid.pointer = byteArray.pointer; pVoid.count = 1;
+	puts(pVoid.pointer);
+
+puts("(unsigned char *) *pVoid;");
+	pVoid.type = byteArray.type;
+	puts(pVoid.value);
+
+// All JSNatives require private data, which must be freed, before the value is garbage collected.
+pVoid.free(); byteArray.free();
+
 
 } catch (e) { puts(e); };
