e46e752121d5c786714018797d64e7a5044fa3f4
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-07-19 11:56:02 -0500

    Base new type system
    I'm revising the type system using reference material from both JS and C.
    
    The hope is to achieve a new type parsing and object modeling system that can be easily bound to c header syntax. The goal here is not to parse C headers, but to take fragments of them and create accurate retranslations.
    
    It's been said that only shooting stars can break the mould. We shall put that theory to the test.
    
    #Build No. 0.0.605

--- core.js
+++ core.js
@@ -1,25 +1,98 @@
 #!bin/jse
 
-echo(JSNative.Type("char **"));
+function Type() {
+	var jsType = typeof argument[0];
+	if (jsType  == 'number' || jsType == 'string') {
+		return Type[argument[0]];
+	}
+}
 
-var type = JSNative.Type;
-var libc = new JSNative.Library("libc.so.6");
+Type.Model = function (name, code, pointer, array, parser) {
+	this.code = code, this.name = name,	this.pointer = pointer,	this.array = array;
+	this.signed = new Boolean((this.code % 2) || this.code == 70 || this.code == 80);
+	this.parse = parser.bind(this);
+	Type[code] = Type[name] = Object.freeze(this);
+}
 
-libc.findSymbol("puts");
-libc.findSymbol("printf");
+Type.sizeOf = function(type) { return Type.sizeOf[type.code]; }
+Type.sizeOf.allocationSize = function(type) { return Type.sizeOf.allocationSize[type.code]; }
 
-var puts = new JSNative.Call("default", type("int"), libc.puts, type("char *"));
-var printf = new JSNative.Call("ellipsis", type("int"), libc.printf, type("char *"));
+Type.Model.prototype = {
+	isEqualTo: function(that) {
+		return (this.code == that.code && this.pointer == that.pointer && this.array == that.array)
+	},
+	get size() { return Type.sizeOf[this.code]; },
+	get allocationSize() { return Type.sizeOf.allocationSize[this.code]; },
+	parse: function () { return this.valueFrom(arguments) },
+	valueOf: function () { return this.code },
+	toString: function() { var build = (this.pointer == true)?"*":''+(this.array == true)?"[]":'';
+		return this.name + (build != '')?" "+build:build
+	},
+}
 
-var i = new JSNative.Value("int", 0);
+new Type.Model("void", 0, 0, 0, function valueFrom() {
+	throw new Error("cannot convert argument"+((arguments.length > 1) ? "s":'')+" to void type");
+	return null;
+});
 
-i.value = puts("Hello world");
+Type.parseDeclaration = function(d) {
+	this.sourceOf = d.replace(/(\*)/g, ' *').replace(/\[/g, ' [').replace(/\n|\s+/g, ' ').replace(/\]\s/g, ']').trim();
+	var stack = this.sourceOf.split(' ');
+	function storage() {
+		if (stack[0] == "const") this.typeConstant = Boolean(stack.shift());
+		typeName.apply(this);
+	}
+	function typeName() {
+		if (stack[0] == "unsigned")	{
+			if (this.typeUnsigned == true) {
+				throw new SyntaxError("");
+				return;
+			}
+			this.typeUnsigned = true; stack.shift();
+			typeName.apply(this); return;
+		}
+		if ((this.typeOf = stack.shift()) == "int") this.typeInteger = true;
+		if (this.typeOf in Type.parseDeclaration.reservedWords) {
+			throw new TypeError("Type.parseDeclaration: typeName: " +this.typeOf+" is a reserved word")
+		}
+		if (this.typeOf.match(/^[_a-zA-Z]+[_a-zA-Z0-9]*$/) == null) {
+			throw new TypeError("Type.parseDeclaration: unable to parse type name: " + this.typeOf);
+			return
+		}
+		if (stack[0] == "int")	{
+			if (this.typeInteger == true) {
+				throw new SyntaxError("");
+				return;
+			}
+			this.typeInteger = true; stack.shift();
+		}
+		indirection.apply(this);
+	}
+	function indirection() {
+		if (stack[0] == "*") {	stack.shift();
+			this.addressOf = true;
+		}
+		array.apply(this);
+	}
+	function array() {
+		if ( ! stack.length > 0) return;
+		if (stack[0] == "*") {	stack.shift();
+			this.arrayOf = true;
+		}
+		if ( ! stack.length > 0) return;
+		if (stack[0].indexOf("[") == 0)	{
+			this.arrayOf = stack.shift();
+		}
+	}
+	storage.apply(this);
+}
 
-printf("%s %i\n", "Hello World", i);
+Type.parseDeclaration.reservedWords = {
+	"const": true, "static": true, "volatile": true, "class": true, "type": true,
+	"instanceof": true, "typeof": true
+};
 
-// test a value cast
-echo(i.pointer.cast("int"))
+var p = new Type.parseDeclaration("void *[]");
 
-// test an array cast
-echo(i.pointer.cast("int", 1)[0])
+for (name in p) echo(name+":", p[name]);
 
