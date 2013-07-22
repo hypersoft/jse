30b6fd54643c384edb1e1d1de7e93df356fa806a
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-07-22 01:09:36 -0500

    Days of Yore
    Modified: core.js

--- core.js
+++ core.js
@@ -1,14 +1,20 @@
 #!bin/jse
 
-Shell = new Class('Shell',
-	{ 
+/* Ye Old Test Class */
+
+Shell = new Class(
+
+	'Shell',
+	{	/* Shell.prototype Object */
 		echo: function() { Shell.echo.apply(this, arguments) } 
 	},
-	{
+	{	/* Shell 'SuperConstructor' methods */
 		create: { value: function() { this.echo("I am baby", Shell.name); }, configurable:true },
 		global: { value: function() { Shell.echo("I am", Shell.name) }, configurable:true },
+		/* an example function */
 		echo: {	value: function() { echo.apply(this, arguments) }, configurable:true }
 	}
+
 );
 
 Shell();
