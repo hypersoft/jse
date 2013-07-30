a267974a904ce150ade36565ffe2805612f7aea4
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-07-30 04:38:31 -0500

    Typing Tutor II
    JSNative.Type is the "typedef" of JSE. Here we present a simple workable model for dealing with the "standard machine types"
    
    The JSNative.Type constructor should probably make room in arguments to allow for unsigned types and pointers to types. There is no method yet provided to combine the type flags that would be needed to compose such a definiton.
    
    On the other hand, every native machine type should have a proper representation, before we get to "userland" and that representation could be used to obtain a "cast" of the desired flags. This is a threefold addendum as it also mitigates the need to typecheck typecodes, while allowing advanced users and scripts to make or break their own eggs.
    
    Modified: core.js

--- core.js
+++ core.js
@@ -1,46 +1,43 @@
 #!bin/jse
 
-(function() {
+(function JSNativeTypeClass() {
 
 var classConstruct = function(code, name){
-	this.type = code;
-	this.name = name;
-	this.constructor[code] = this;
-	this.constructor[name] = this;
+	this.type = code, this.name = name;
+	// type size is a constant defined on the 'type definition' for optimal performance
+	Object.defineProperty(this, 'size', {value: JSNative.api.getTypeSize(this), enumerable:true})
+	// The first definition of a code is its "native representation"
+	// don't overwrite codes that have been written.
+	if (! code in this.constructor) this.constructor[code] = this;
+	// The name of a type is its alias
+	// don't overwrite an alias that has been written
+	if (name in this.constructor) {
+		throw new InvokeError("TypeError", "type name '"+name+"' is already registered");
+	} else {
+		this.constructor[name] = this;
+	}
 }
 
-var classInvoke = function(code) {
-	return this.Type[code];
-}
+var classInvoke = function(code) {return this.Type[code]}
 
 new JSNative.Class
 (
 	"JSNative.Type",
 	{ /* class instance methods (constructor prototype) */
+		toString: function() {return this.name},
+		valueOf: function() {return this.type}
 	},
 	{ /* class methods (constructor methods) */
 		classConstruct:classConstruct,
 		classInvoke:classInvoke,
+		sizeOf: function(typeId) {return this[typeId].size},
 	}
 )
-
-Object.defineProperties(JSNative.Type.prototype, {
-	size:{
-		get:function(){return JSNative.api.getTypeSize(this.type)},
-		enumerable:true
-	},
-	toString:{
-		value: function() {return this.name}
-	},
-	valueOf:{
-		value: function() {return this.type}
-	}
-})
 
 })();
 
 char = new JSNative.Type(8, "char");
 
-echo(JSNative.Type("char"))
+echo("sizeOf(char) ===", JSNative.Type.sizeOf("char"))
 
 
