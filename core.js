f6b0b938792c4b7f74930dcf315e4279ac6ceffb
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-07-30 03:16:38 -0500

    Prime Territory
    This little rendition, details the current class scheme, and presents a plausible prototype for the JSNative.Type interface.
    
    Modified: core.js

--- core.js
+++ core.js
@@ -1,17 +1,46 @@
 #!bin/jse
 
-//new JSNative.Class(
-//	"foo",
-//	{ /* class instance methods (constructor prototype) */
-//		classInitialize:function(){echo('instance initialize')},
-//		classInvoke:function(){echo('instance invoked')},
-//	},
-//	{ /* class methods (constructor methods) */
-//		classConstruct:function(){echo('constructor called')},
-//		classInvoke:function(){echo('class invoked')},
-//	}
-//)
+(function() {
 
-//b = new foo();
+var classConstruct = function(code, name){
+	this.type = code;
+	this.name = name;
+	this.constructor[code] = this;
+	this.constructor[name] = this;
+}
 
-echo("sizeof(bool) ===", JSNative.api.getTypeSize(4))
+var classInvoke = function(code) {
+	return this.Type[code];
+}
+
+new JSNative.Class
+(
+	"JSNative.Type",
+	{ /* class instance methods (constructor prototype) */
+	},
+	{ /* class methods (constructor methods) */
+		classConstruct:classConstruct,
+		classInvoke:classInvoke,
+	}
+)
+
+Object.defineProperties(JSNative.Type.prototype, {
+	size:{
+		get:function(){return JSNative.api.getTypeSize(this.type)},
+		enumerable:true
+	},
+	toString:{
+		value: function() {return this.name}
+	},
+	valueOf:{
+		value: function() {return this.type}
+	}
+})
+
+})();
+
+char = new JSNative.Type(8, "char");
+
+echo(JSNative.Type("char"))
+
+
