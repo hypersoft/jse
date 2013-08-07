6bbc2e90a2fcef662e2a7925eff7c30081cffccd
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-08-07 03:10:11 -0500

    Migrated type constants from binary source
    Modified: core.js
    Modified: src/JSNative/JSNativeType.js

--- core.js
+++ core.js
@@ -1,5 +1,17 @@
 #!bin/jse
 
+JSNative.api.typeVoid = 2,
+JSNative.api.typeBoolean = 4,
+JSNative.api.typeChar = 8,
+JSNative.api.typeShort = 16,
+JSNative.api.typeInt = 32,
+JSNative.api.typeLong = 64,
+JSNative.api.typeLongLong = 128,
+JSNative.api.typeFloat = 256,
+JSNative.api.typeDouble = 512,
+JSNative.api.typeFunction = 1024,
+JSNative.api.typeEllipsis = 2048;
+
 /* static type code definition */
 
 /*
--- src/JSNative/JSNativeType.js
+++ src/JSNative/JSNativeType.js
@@ -1,15 +1,2 @@
-JSNative.api.typeVoid = 2,
-JSNative.api.typeBoolean = 4,
-JSNative.api.typeChar = 8,
-JSNative.api.typeShort = 16,
-JSNative.api.typeInt = 32,
-JSNative.api.typeLong = 64,
-JSNative.api.typeLongLong = 128,
-JSNative.api.typeFloat = 256,
-JSNative.api.typeDouble = 512,
-JSNative.api.typePointer = 1024,
-JSNative.api.typeArray = 2048,
-JSNative.api.typeUnsigned = 4096,
-JSNative.api.typeEllipsis = 8192;
-
+//
 
