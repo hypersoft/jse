3f123f23625c627f67863ee34b7fe3916f5e8d61
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-09-30 16:27:49 -0500

    Runtime maintenance commentary
    Modified: core.js

--- core.js
+++ core.js
@@ -3,7 +3,14 @@
 var lib = new SharedLibrary("libc.so.6");
 var cvm = new CallVM(8);
 var utf8 = js.native.toUTF8("Hello World");
-cvm.push([4], utf8); /* 4 == int_t */
+cvm.push([4], utf8);      /* 4 == int_t */
 cvm.call(4, lib.find("puts"));
 
+// yeah.. so clean that shit up...
 
+js.native.freeUTF8(utf8);
+cvm.free();
+lib.unload();
+
+// exceptions will hurt you unless you develop an allocation stack to keep track of pointers
+// and catch those exceptions!
