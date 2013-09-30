2e29f673d8e5809c70cd5d99ac1e8be1434d5e2d
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-09-30 16:17:01 -0500

    Fixed oopsey daisey (CCP undo... typo)
    Native UTF8 string print 100% from JS in core.js
    
    Modified: bin/buildnum
    Modified: core.js
    Modified: src/JSTools/Native.inc

--- core.js
+++ core.js
@@ -1,12 +1,9 @@
 #!bin/jse
 
-function NativeFunction(sym, retType, paramTypeArray) {
-
-}
-
 var lib = new SharedLibrary("libc.so.6");
 var cvm = new CallVM(8);
-cvm.push([4], 77); /* 4 == int_t */
-cvm.call(4, lib.find("putchar"));
+var utf8 = js.native.toUTF8("Hello World");
+cvm.push([4], utf8); /* 4 == int_t */
+cvm.call(4, lib.find("puts"));
 
 
