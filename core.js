651fa06101f23dc277ae30c1d9e96cb741c86fd9
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-11 05:10:25 -0500

    push
    Modified: core.js
    Modified: src/JSTools/Native.inc

--- core.js
+++ core.js
@@ -3,6 +3,11 @@
 // exceptions will hurt you unless you develop an allocation stack to keep track of pointers
 // and catch those exceptions!
 
+// Up next..
+// NativeArray
+// Allocator
+// NativeFunction
+
 var lib = new SharedLibrary(); // no name gets symbols from JSE ...
 
 var cvm = new CallVM(8);
@@ -13,5 +18,5 @@ cvm.push([4, 4], utf8, utf32);      /* 4 == int_t */
 cvm.call(4, lib.find("printf"));
 
 // yeah.. so clean that shit up...
-js.native.free(utf8, utf32, cvm, lib);
+js.native.free(utf8, utf32, cvm);
 
