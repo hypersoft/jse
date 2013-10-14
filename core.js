add51170d085a948ad3898d9f2412ae787773e53
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-14 15:32:51 -0500

    Fuck gits

--- core.js
+++ core.js
@@ -1,22 +1,18 @@
 #!bin/jse
 
 var echo = new Command('echo', '-E');
-
-var Address = function(){} // placeholder for definition
-Address = js.native.container(Object.defineProperties(function Address(p) {
-	var ptr = js.native.instance(Address, p);
-	return ptr;
-}, {
-	deallocate:{value:true}
-}));
+var print = new Command('echo', '-En');
+var printf = new Command('printf', '--');
 
 // exceptions will hurt you unless you develop an allocation stack to keep track of pointers
 // and catch those exceptions!
 
-echo("Container type: "+js.classOf(new Address(0)));
+var int = js.type.int;
+var native = js.call.native;
 
-// the program will now loop until forever to show you that garbage collect
-// DOES NOT FINALIZE CLASS INSTANCE EVER!
-// SHOULD GARBAGE COLLECT CALL FINALIZE, YOU WILL BE NOTIFIED VIA STDOUT.
+var puts = new Procedure(js.engine, 'puts', native, [int, js.type.address(js.type.utf8)]);
 
+print("Hello to you "), puts(js.user.name);
+
+exit(0);
 
