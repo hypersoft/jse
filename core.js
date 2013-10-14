9d76ac72acb5d75ed758721cb2cf22d6b2e0e1b3
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-14 13:33:47 -0500

    WebKit classFinalize Issues
    This commit demonstrates by inifinite loop, no matter how many times you force garbage collection on a host object with no scope, WebKit DOES NOT call class finalize. Which means we cannot reliably use this as a method for automatically freeing up allocated resources. Searching the web for a proof of concept as a working prototype turns up no results.
    
    Perhaps there is a "magic" incantation one must perform to get this working properly. Even if that is the case, it certainly isn't documented.
    
    WANTED: PROOF OF WORK PERFORMED: WEBKIT CLASS FINALIZE
    
    Modified: bin/buildnum
    Modified: core.js
    Modified: main.c
    Modified: src/JSTools/Native.inc

--- core.js
+++ core.js
@@ -1,19 +1,24 @@
 #!bin/jse
 
-var echo = new Command('echo', '-E');
-var print = new Command('echo', '-En');
-var printf = new Command('printf', '--');
+var Address = function(){} // placeholder for definition
+Address = js.native.container(Object.defineProperties(function Address(p) {
+	var ptr = js.native.instance(Address, p);
+	return ptr;
+}, {
+	deallocate:{value:true}
+}));
 
 // exceptions will hurt you unless you develop an allocation stack to keep track of pointers
 // and catch those exceptions!
 
-var int = js.type.int;
-var utf8p = js.type.utf8 | js.type.pointer;
-var native = js.call.native;
+b = new Address(0);
 
-var puts = new Procedure(js.engine, 'puts', native, [int, utf8p]);
+echo("Container type: "+js.classOf(b));
 
-print("Hello to you "), puts(js.user.name);
+delete b;
 
-exit(0);
+// the program will now loop until forever to show you that garbage collect
+// DOES NOT FINALIZE CLASS INSTANCE EVER!
+// SHOULD GARBAGE COLLECT CALL FINALIZE, YOU WILL BE NOTIFIED VIA STDOUT.
+
 
