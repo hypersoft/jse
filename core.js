4b28196c752c9ec73c424940f7557565b5bf66e9
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-14 13:39:45 -0500

    More of the previous...
    This time only on the stack, whose scope and reference is immediately invalidated. post call.
    
    This shit is definitely NOT WORKING AS ADVERTISED.
    
    Modified: core.js

--- core.js
+++ core.js
@@ -13,11 +13,7 @@ Address = js.native.container(Object.defineProperties(function Address(p) {
 // exceptions will hurt you unless you develop an allocation stack to keep track of pointers
 // and catch those exceptions!
 
-b = new Address(0);
-
-echo("Container type: "+js.classOf(b));
-
-delete b;
+echo("Container type: "+js.classOf(new Address(0)));
 
 // the program will now loop until forever to show you that garbage collect
 // DOES NOT FINALIZE CLASS INSTANCE EVER!
