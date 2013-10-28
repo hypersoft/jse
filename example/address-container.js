afcf76c03a898384a5f7a7400d681b454f23fa5b
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-28 18:04:19 -0500

    Improved address container
    Modified: example/address-container.js

--- example/address-container.js
+++ example/address-container.js
@@ -3,21 +3,16 @@ var Address = js.extendPrototype(function Address(v, t){
 	var o = js.native.instance(Address.container, 0);
 	js.native.setPrototype(o, Address.prototype);
 	if (t != undefined) o.type = (typeof t == 'string')?js.type[t]:t;
-	if (v != undefined) o.address = parseInt(v);
+	if (v != undefined) o['*'] = parseInt(v);
 	return o;
 }, {
 	container: null, name: "Address", prototype: {
 		'&': null, 'value': null, type: js.type.void, address: 0
 	},
-	convert: function(type) {
-		return this.address;
-	},
 	set: function(name, value) {
-		if (!isNaN(p = parseInt(name))) {
-			return js.native.address.write(this.address + (p * this.size), this.type, this.value);
+		if (!isNaN(i = parseInt(name))) {
+			return js.native.address.write(this['*'] + (i * this.size), this.type, this.value);
 		}
-		if (name == 'value') return js.native.address.write(this.address, this.type, value);
-		if (name == '&') return this.address = value;
 		if (name == 'type') {
 			this.type = (typeof value == 'string')?js.type[value]:value;
 			this.size = js.native.typeSize(this.type);
@@ -25,14 +20,13 @@ var Address = js.extendPrototype(function Address(v, t){
 		return null;
 	},
 	get: function(name) {
-		if (name == 'value') return js.native.address.read(this.address, this.type);
-		if (name == '&') return this.address;
-		if ((index = name.match(/\&([0-9]+)/)) != null) {
-			index = index[1];
-			return (this.address + (index * this.size));
-		}
 		if (!isNaN(i = parseInt(name))) {
-			return js.native.address.read((this.address + (i * this.size)), this.type);
+			return js.native.address.read((this['*'] + (i * this.size)), this.type);
+		}
+		var index = name.match(/^\&([0-9]+)$/);
+		if (index != null) {
+			index = index[1];
+			return (this['*'] + (index * this.size));
 		}
 		return null;
 	}
@@ -44,7 +38,7 @@ Address.container = js.native.container(Address);
 /* ------------ */
 
 // print out arguments
-var echo = new Command('echo');
-var i, argv = Address(js.run.argv, "utf8 *");
-for (i = 0; i < js.run.argc; i++) echo(js.native.fromUTF8(argv[i]));
+var argv = Address(js.run.argv, "utf8 *");
+var i, puts = new Procedure("jse", "puts", "native", ["int", "utf8 *"]);
+for (i = 0; i < js.run.argc; i++) puts(argv[i]);
 
