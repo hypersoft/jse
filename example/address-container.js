3458e3f9d31ba82005f4cebd9bc6c99887612b2b
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-29 00:54:05 -0500

    improved address example

--- example/address-container.js
+++ example/address-container.js
@@ -1,16 +1,15 @@
 
 // All Number objects have the 'toAddress' convertor...
-var argv = js.run.argv.toAddress("utf8 *", js.run.argc);
+// All Number objects have the 'toHex' convertor...
 
+var argv = js.run.argv.toAddress("utf8 *", js.run.argc);
 var i, puts = new Procedure("jse", "puts", "native", ["int", "utf8 *"]);
 
 // In C `&' means to get address instead of value ...
 // the address returned is a primitve value
 
-// All Number objects have the 'toHex' convertor...
-
-// Print out &argv in hex
-puts(argv['&'].toHex())
+// Print out &argv as object->string
+puts(argv['&'].toAddress())
 
 // Print out &argv[0] in hex
 puts(argv['&0'].toHex())
