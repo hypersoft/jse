b1fdb31d63782e1c09954d452fca859eb30a004d
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-28 20:22:19 -0500

    Moved Address to internal
    Modified: bin/buildnum
    Modified: example/address-container.js
    Modified: src/JSTNative.js

--- example/address-container.js
+++ example/address-container.js
@@ -1,49 +1,7 @@
 
-var Address = js.extendPrototype(function Address(v, t){
-	var o = js.native.instance(Address.container, 0);
-	js.native.setPrototype(o, Address.prototype);
-	if (t != undefined) o.type = (typeof t == 'string')?js.type[t]:t;
-	if (v != undefined) o['*'] = parseInt(v);
-	return o;
-}, {
-	container: null, name: "Address", prototype: { type: js.type.void, address: 0,
-		toString: function(){ return '[object Address 0x'+this['*'].toString(16)+']'},
-		valueOf: function(){ return this['*']; },
-	},
-	set: function(name, value) {
-		if (!isNaN(i = parseInt(name))) {
-			return js.native.address.write(this['*'] + (i * this.size), this.type, this.value);
-		}
-		if (name == 'value') {
-			return js.native.address.write(this['*'], this.type, this.value);			
-		}
-		if (name == 'type') {
-			this.type = (typeof value == 'string')?js.type[value]:value;
-			this.size = js.native.typeSize(this.type);
-		}
-		return null;
-	},
-	get: function(name) {
-		if (!isNaN(i = parseInt(name))) {
-			return js.native.address.read((this['*'] + (i * this.size)), this.type);
-		}
-		var index = name.match(/^\&([0-9]+)$/);
-		if (index != null) {
-			index = index[1];
-			return (this['*'] + (index * this.size));
-		}
-		return null;
-	}
-})
-
-Address.prototype.constructor = Address;
-Address.container = js.native.container(Address);
-
-/* ------------ */
-
 // print out arguments
-var argv = Address(js.run.argv, "utf8 *");
+var argv = new Address(js.run.argv, "utf8 *");
 var i, puts = new Procedure("jse", "puts", "native", ["int", "utf8 *"]);
-puts(argv)
+
 for (i = 0; i < js.run.argc; i++) puts(argv[i]);
 
