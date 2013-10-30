5e1b8d4f2d1dc7fcb1a3f0903b1df0787e5453eb
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-10-30 10:04:06 -0500

    Added closure example code

---
+++ example/callback.js
@@ -0,0 +1,22 @@
+
+// This is not a very good example!
+// but it demonstrates a working theory...
+
+var echo = new Command('echo', '-E');
+
+// create the function to call..
+function target(i) {
+	return echo("callback target was called with:", i);
+}
+
+
+// setup the callback & prototype...
+var cb = js.native.callback(this, target, [js.type.int, js.type.int]);
+
+// create a native procedure to test the callback...
+var test = Procedure("jse", "jsNativeCallbackTest", "native", [js.type.int, js.type.pointer, js.type.int]);
+
+test(cb, 12);
+
+cb.free();
+
