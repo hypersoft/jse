d257da10efec344155453518f9872657b85162a5
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-11-05 16:47:37 -0600

    Catskin
    Demo on pointers and strings + versioning instrumentation
    
    Modified: core.js

--- core.js
+++ core.js
@@ -4,7 +4,21 @@
 //var print = new Command('echo', '-En');
 //var printf = new Command('printf', '--');
 
+printVersion = Procedure(
+	"jse", "int", "printf", ['utf8 *', 'utf8 *', 'utf8 *', 'utf8 *']).bind(
+	this, "%s JSE %s v%s\n", js.vendor, js.codename, js.version
+);
 
-Procedure("jse", "int", "printf", ['utf8 *', 'utf8 *', 'utf8 *', 'utf8 *'])
-("%s JSE %s v%s\n", js.vendor, js.codename, js.version);
+printVersion();
+
+// More than one way to skin a 'cat'
+var printf = new Command('printf', '--');
+var vendor = js.native.fromUTF8(js.vendor); // these are JS managed string copies ;)
+var codename = js.native.fromUTF8(js.codename);
+var version = js.native.fromUTF8(js.version);
+
+printf("%s JSE %s v%s\n", vendor, codename, version);
+
+// Since 'Commands' expect string parameters, the ellipsis interpretation is strictly
+// implied.
 
