2557946de11ecaf9b8ae18020a29a350a4655efd
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-11-11 12:46:34 -0600

    Typo correction
    Modified: core.js

--- core.js
+++ core.js
@@ -5,5 +5,5 @@ var int = native.type.int;
 
 var printf = Procedure(native.engine, int, "printf", [utf8p, '...'])
 
-printf("%s JSE %s %s\n", js.vendor, js.codename, js.version);
+printf("%s JSE %s v%s\n", js.vendor, js.codename, js.version);
 
