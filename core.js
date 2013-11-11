382cd9e087b15afb9ff61184af75b7dc4c71c436
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-11-11 01:13:49 -0600

    Revelation
    Pointers are numbers too. lol
    
    Modified: core.js

--- core.js
+++ core.js
@@ -4,9 +4,12 @@ var utf8p = native.type.utf8.constant.pointer;
 var int = native.type.int;
 
 // We need some pointers to setup as 'type qualified values'
-var vendor = utf8p.value(js.vendor);
-var codename = utf8p.value(js.codename);
-var version = utf8p.value(js.version);
+//var vendor = utf8p.value(js.vendor);
+//var codename = utf8p.value(js.codename);
+//var version = utf8p.value(js.version);
+
+// Actually, on systems where int is the same size as pointer, you
+// don't need to mark the pointer types explicit.
 
 // we can demonstrate automatic utf8 pointer conversion of String objects
 var auto = "Featuring JavaScriptCore"
@@ -15,5 +18,5 @@ var auto = "Featuring JavaScriptCore"
 
 var printf = Procedure(native.engine, int, "printf", [utf8p, '...'])
 
-printf("%s JSE %s %s %s\n", vendor, codename, version, auto);
+printf("%s JSE %s %s %s\n", js.vendor, js.codename, js.version, auto);
 
