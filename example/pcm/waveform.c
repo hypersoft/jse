116526c8c29aa42a5720647e31bd8951c058ff4f
Triston J. Taylor <pc.wiz.tt@gmail.com>
2013-11-26 11:05:37 -0600

    Revised PCM Wave Generator
    Added a mixer supporting three different styles of input. See comments for details.
    Upgraded example to demo the current mixer features.
    
    Modified: example/pcm/waveform.c
    Modified: example/pcm/wavegen

--- example/pcm/waveform.c
+++ example/pcm/waveform.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <stdarg.h>
 
 double radians = (M_PI * 2);
 
@@ -12,6 +14,14 @@ typedef struct WaveForm {
 	double frequency;
 	double amplitude;
 } WaveForm;
+
+typedef struct WaveBuffer { // for customized WaveData
+	short * data;
+	size_t length;
+	size_t loop;
+	double amplitude;
+} WaveBuffer;
+
 
 WaveForm * new_wave_form(double freq, double amp) {
 	WaveForm * result = malloc(sizeof(WaveForm));
@@ -21,26 +31,86 @@ WaveForm * new_wave_form(double freq, double amp) {
 
 void wave_form_free(WaveForm * wave) { free(wave); }
 
-// period == fractional time (seconds)
-short wave_form_sample(WaveForm * wave, double period) {
-    return (short) wave->amplitude * sin(radians * wave->frequency * period);
+WaveBuffer * new_wave_buffer(size_t length, double amplitude) {
+	WaveBuffer * result = malloc(sizeof(WaveBuffer));
+	result->amplitude = amplitude; result->loop = 0;
+	result->data = malloc(length << 2); result->length = length;
+	return result;
 }
 
-void wave_form_sample_write(WaveForm * wave, double period, FILE * file) {
-    double sample = wave->amplitude * sin(radians * wave->frequency * period);
+void wave_buffer_free(WaveBuffer * wave) { free(wave->data); free(wave); }
+
+// phase == fractional time (seconds)
+short wave_form_sample(WaveForm * wave, double phase) {
+    return (short) wave->amplitude * sin(radians * wave->frequency * phase);
+}
+
+short wave_form_mix_samples(short a, short b) {
+	int target = a + b;
+	if (target > SHRT_MAX) return SHRT_MAX;
+	if (target < SHRT_MIN) return SHRT_MIN;
+	else return (short) target;
+}
+
+
+void wave_form_sample_write(WaveForm * wave, double phase, FILE * file) {
+    double sample = wave->amplitude * sin(radians * wave->frequency * phase);
     short s16 = (short)sample; fwrite(&s16, sizeof(short), 1, file);
 }
 
 // duration == fractional time (seconds)
 void wave_form_stream_sample(WaveForm * wave, double duration, size_t rate, FILE * file) {
 
-	double t;
-	for (t = 0; t < duration; t += 1.0/rate) {
-		double sample = wave->amplitude * sin(radians * wave->frequency * t);
+	double phase;
+	for (phase = 0; phase < duration; phase += 1.0/rate) {
+		double sample = wave->amplitude * sin(radians * wave->frequency * phase);
 		short s16 = (short)sample;
 		fwrite(&s16, sizeof(short), 1, file);
 	}
 
+}
+
+WaveBuffer * wave_form_buffer(WaveForm * wave, double duration, size_t rate) {
+	size_t length = (size_t)(duration * rate);
+	WaveBuffer * buffer = new_wave_buffer(length, wave->amplitude);
+	double phase;
+	for (phase = 0; phase < duration; phase += 1.0/rate) {
+		double sample = wave->amplitude * sin(radians * wave->frequency * phase);
+		buffer->data[buffer->loop++] = (short)sample;
+	}
+	buffer->loop = 0;
+	return buffer;
+}
+
+void wave_form_stream_mix(double duration, size_t rate, FILE * file, char * fmt, ...) {
+
+	double phase, sample; va_list ap; WaveForm * wave; WaveBuffer * buffer;
+	size_t index, sampleIndex = 0; char type; short s16;
+	for (phase = 0; phase < duration; phase += 1.0/rate, sampleIndex++) {
+		va_start(ap, fmt); index = 0;
+		while ((type = fmt[index++]) != 0) {
+			if (type == 'w') {
+				wave = va_arg(ap, WaveForm *);
+				sample = wave->amplitude * sin(radians * wave->frequency * phase);
+				s16 = (short)sample;
+				fwrite(&s16, sizeof(short), 1, file);
+			} else if (type == 'b') {
+				buffer = va_arg(ap, WaveBuffer *);
+				if (sampleIndex < buffer->length) {
+					s16 = buffer->data[sampleIndex];
+				} else s16 = 0;
+				fwrite(&s16, sizeof(short), 1, file);
+			} else if (type == 'l') {
+				buffer = va_arg(ap, WaveBuffer *);
+				if (buffer->loop > buffer->length) buffer->loop = 0;
+				if (buffer->data) s16 = buffer->data[buffer->loop++];
+				else s16 = 0; fwrite(&s16, sizeof(short), 1, file);
+			} else {
+				fprintf(stderr, "TypeError: '%c' is not a valid WaveForm argument\n", type);
+				return; 
+			}
+		}
+	}
 }
 
 
