f45326953e18074566acb51764315d3faa6eb15a
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-17 02:46:54 -0600

    Rebuild MachineTypes and Export an API

---
+++ include/MachineType.h
@@ -0,0 +1,11 @@
+#ifndef JSE_H
+#include "jse.h"
+#ifndef MACHINE_TYPE_C
+#define EXPORTING extern
+#else
+#define EXPORTING
+#endif
+EXPORTING double MachineTypeRead(JSContext ctx, void * address, size_t element, unsigned flags, JSValue exception);
+EXPORTING bool MachineTypeWrite(JSContext ctx, void * address, size_t element, double value, unsigned flags, JSValue exception);
+#endif
+
--- plugin/MachineType.c
+++ plugin/MachineType.c
@@ -1,7 +1,10 @@
-#include <jse.h>
+#define MACHINE_TYPE_C
+
+#include <MachineType.h>
+
 #define MTCLASS "MachineType"
 static int loadCount = 0;
-JSClass MachineTypeClass = NULL;
+static JSClass MachineTypeClass = NULL;
 
 static JSClassDefinition MachineTypeClassDefinition = {
 	0,										/* Version, always 0 */
@@ -24,15 +27,58 @@ static JSClassDefinition MachineTypeClassDefinition = {
 	NULL									/* Object Convert To Type */
 };
 
-static JSValue MachineTypeObjectConstructor (JSContext ctx, JSObject function, JSObject this, size_t argc, const JSValue argv[], JSValue * exception)
+static JSValue JSMachineTypeConstructor (JSContext ctx, JSObject function, JSObject this, size_t argc, const JSValue argv[], JSValue * exception)
 {
 	JSObject machineTypeConstructor = (JSObject) JSObjectGetUtf8Property(ctx, function, "create");
 	JSObjectCallAsFunction(ctx, machineTypeConstructor, this, argc, argv, exception);
 	return this;
 }
 
+double MachineTypeRead(JSContext ctx, void * address, size_t element, unsigned code, JSValue exception) {
 
-JSValue machineTypeRead(JSContext ctx, JSObject function, JSObject this, size_t argc, const JSValue argv[], JSValue * exception)
+		unsigned
+			width = (code & (1|2|4|8));
+		bool 
+			sign = (code & 1024),
+				floating = (code & 2048),
+					pointer = (code & 512),
+						utf = (code & 4096); // <-- this won't be used, as utfN is just an alias for get charcode
+
+		if (pointer) {
+			return ((uintptr_t *)address)[element];
+		} else if (floating) {
+			if (width == 4) return ((float*)address)[element];
+			else if (width == 8) return ((double*)address)[element];
+			else goto bad_float_request;
+		}
+
+		if (address && width) {
+			if (sign) {
+				if (width == 1) return ((signed char *)address)[element];
+				else if (width == 2) return ((signed short *)address)[element];
+				else if (width == 4) return ((signed long *)address)[element];
+				else if (width == 8) return ((signed long long *)address)[element];
+			} else {
+				if (width == 1) value = ((unsigned char *)address)[element];
+				else if (width == 2) return ((unsigned short *)address)[element];
+				else if (width == 4) return ((unsigned long *)address)[element];
+				else if (width == 8) return ((unsigned long long *)address)[element];
+			}
+		}
+
+bad_type_request:
+
+	THROWING_EXCEPTION(JSExceptionFromUtf8(ctx, JSE_ERROR_CTOR, "failed to read from address %p with a width of %i", address, width));
+	return 0;
+
+bad_float_request:
+
+	THROWING_EXCEPTION(JSExceptionFromUtf8(ctx, JSE_ERROR_CTOR, "failed to read floating value from address %p with a width of %i", address, width));
+	return 0;
+
+}
+
+static JSValue JSMachineTypeRead(JSContext ctx, JSObject function, JSObject this, size_t argc, const JSValue argv[], JSValue * exception)
 {
 	// address, element?
 	if (argc < 1 || argc > 2) {
@@ -40,47 +86,75 @@ JSValue machineTypeRead(JSContext ctx, JSObject function, JSObject this, size_t 
 	}
 
 	void * address = (void*)(uintptr_t)JSValueToNumber(ctx, argv[0], NULL);
-	long long element = (argc > 1)?JSValueToNumber(ctx, argv[1], NULL):0;
+	size_t element = (argc > 1)?JSValueToNumber(ctx, argv[1], NULL):0;
+	unsigned code = JSValueToNumber(ctx, (JSValue) this, NULL);
 
-	unsigned code = JSValueToNumber(ctx, (JSValue) this, NULL),
-			 width = (code & (1|2|4|8));
+	JSValue result = JSValueFromNumber(ctx, MachineTypeRead(ctx, address, element, code, exception));
 
-	bool sign = (code & 1024),
-		floating = (code & 2048),
-		pointer = (code & 512);
+	if (exception && *exception) return JSValueMakeNull(ctx);
 
-	if (pointer) sign = floating = 0, width = sizeof(void*);
+	return result;
 
-	double value = 0;
-
-	if (address && width) {
-		if (sign) {
-			if (width == 1) value = ((signed char *)address)[element];
-			else if (width == 2) value = ((signed short *)address)[element];
-			else if (width == 4) value = ((signed long *)address)[element];
-			else if (width == 8) value = ((signed long long *)address)[element];
-			else g_assert_not_reached();
-		} else {
-			if (floating) {
-				if (width == 4) value = ((float *)address)[element];
-				else if (width == 8) value = ((double *)address)[element];
-				else g_assert_not_reached();
-			} else {
-				if (width == 1) value = ((unsigned char *)address)[element];
-				else if (width == 2) value = ((unsigned short *)address)[element];
-				else if (width == 4) value = ((unsigned long *)address)[element];
-				else if (width == 8) value = ((unsigned long long *)address)[element];
-				else g_assert_not_reached();
-			}
-		}
-		JSValue nargv[] = {JSValueFromNumber(ctx, value), 0};
-		return JSObjectCallAsFunction(ctx, this, this, 1, nargv, exception);
-	} else {
-		return JSValueMakeUndefined(ctx);
-	}
 }
 
-JSValue machineTypeWrite(JSContext ctx, JSObject function, JSObject this, size_t argc, const JSValue argv[], JSValue * exception)
+bool MachineTypeWrite(JSContext ctx, void * address, size_t element, double value, unsigned code, JSValue exception) {
+
+		unsigned
+			width = (code & (1|2|4|8));
+		bool 
+			sign = (code & 1024),
+				floating = (code & 2048),
+					constant = (code & 256),
+						pointer = (code & 512),
+							utf = (code & 4096);
+
+		if (constant) return true;
+
+		double value = 0;
+
+		if (JSValueIsNumber(ctx, data)) {
+			value = JSValueToNumber(ctx, data, exception);
+		} else if (JSValueIsString(ctx, data)) {
+			JSStringRef jsStr = JSValueToString(ctx, data, exception);
+			short * str = (void*) JSStringGetCharactersPtr(jsStr);
+			int len = JSStringGetLength(jsStr);
+			if (len == 1) {
+				value = str[0];
+			} else {
+				JSExceptionThrowUtf8(ctx, "TypeError", exception, "trying to set address member %s to a multi-character-string", name);
+			}
+		} else {
+			value = JSValueToNumber(ctx, data, exception);
+		}
+
+		if (exception && *exception) return true;
+
+		if (pointer) sign = floating = 0, width = sizeof(void*);
+
+		if (address && width && constant == 0) {
+			if (floating) {
+				if (width == 4) ((float *)address)[element] = value;
+				else if (width == 8) ((double *)address)[element] = value;
+				else goto oops;
+			} else {
+				if (width == 1) ((unsigned char *)address)[element] = value;
+				else if (width == 2) ((unsigned short *)address)[element] = value;
+				else if (width == 4) ((unsigned long *)address)[element] = value;
+				else if (width == 8) ((unsigned long long *)address)[element] = value;
+				else goto oops;
+			}
+		}
+
+		return true;
+
+oops:
+
+	THROWING_EXCEPTION(JSExceptionFromUtf8(ctx, JSE_ERROR_CTOR, "failed to write to address %p with a width of %i", address, width));
+	return false;
+
+}
+
+JSValue JSMachineTypeWrite(JSContext ctx, JSObject function, JSObject this, size_t argc, const JSValue argv[], JSValue * exception)
 {
 	// address, value
 	// address, element, value
@@ -93,33 +167,15 @@ JSValue machineTypeWrite(JSContext ctx, JSObject function, JSObject this, size_t
 	if (argc == 2) value = JSValueToNumber(ctx, argv[1], NULL);
 	else if (argc > 2)
 		element = JSValueToNumber(ctx, argv[1], NULL),
-		value = JSValueToNumber(ctx, argv[2], NULL);
-	unsigned code = JSValueToNumber(ctx, (JSValue) this, NULL),
-			 width = (code & (1|2|4|8));
-
-	bool sign = (code & 1024),
-		floating = (code & 2048),
-		constant = (code & 256),
-		pointer = (code & 512);
-
-	if (pointer) sign = floating = 0, width = sizeof(void*);
-
-	if (address && width && constant == 0) {
-		if (floating) {
-			if (width == 4) ((float *)address)[element] = value;
-			else if (width == 8) ((double *)address)[element] = value;
-			else g_assert_not_reached();
-		} else {
-			if (width == 1) ((unsigned char *)address)[element] = value;
-			else if (width == 2) ((unsigned short *)address)[element] = value;
-			else if (width == 4) ((unsigned long *)address)[element] = value;
-			else if (width == 8) ((unsigned long long *)address)[element] = value;
-			else g_assert_not_reached();
-		}
-		return argv[1];
-	} else {
-		return JSValueMakeUndefined(ctx);
+			value = JSValueToNumber(ctx, argv[2], NULL);
+	unsigned code = JSValueToNumber(ctx, (JSValue) this, NULL);
+	
+	if (MachineTypeWrite(ctx, address, element, value, code, exception)) {
+		return JSValueFromNumber(ctx, value);
 	}
+
+	return JSValueMakeUndefined(ctx);
+	,
 }
 
 JSValue load(JSContext ctx, char * path, JSObject object, JSValue * exception)
@@ -133,12 +189,12 @@ JSValue load(JSContext ctx, char * path, JSObject object, JSValue * exception)
 	JSObject MachineType;
 	JSObjectSetUtf8Property(ctx, object, MTCLASS,
 		(JSValue) (MachineType = JSConstructorCreate(
-			ctx, MTCLASS, MachineTypeClass, MachineTypeObjectConstructor
+			ctx, MTCLASS, MachineTypeClass, JSMachineTypeConstructor
 		)), 0
 	);
 
-	JSObjectCreateFunction(ctx, MachineType, "read", machineTypeRead);
-	JSObjectCreateFunction(ctx, MachineType, "write", machineTypeWrite);
+	JSObjectCreateFunction(ctx, MachineType, "read", JSMachineTypeRead);
+	JSObjectCreateFunction(ctx, MachineType, "write", JSMachineTypeWrite);
 
 	// make it easy to define the platform pointer type with this [hidden] static ref.
 	JSObjectSetUtf8Property(ctx, MachineType, "ptrSize", JSValueFromNumber(ctx, sizeof(uintptr_t)), 0);
