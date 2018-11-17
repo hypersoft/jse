7fd6b54a1be7b0dca128e519ddc4c6b94fd94402
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-17 03:04:35 -0600

    Rebuilt MachineType

--- include/MachineType.h
+++ include/MachineType.h
@@ -2,7 +2,7 @@
 #include "jse.h"
 #ifndef MACHINE_TYPE_C
 extern double MachineTypeRead(JSContext ctx, void * address, size_t element, unsigned flags, JSValue exception);
-extern bool MachineTypeWrite(JSContext ctx, void * address, size_t element, double value, unsigned flags, JSValue exception);
+extern bool MachineTypeWrite(JSContext ctx, void * address, size_t element, JSValue data, unsigned flags, JSValue exception);
 #else
 #endif
 #endif
