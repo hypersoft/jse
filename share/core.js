9a3193731715d59667532bc7a8d28e7d3e821f4e
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-18 16:05:43 -0600

    Add String.prototype.chomp (perl-likeness) in string I/O processing.

--- share/core.js
+++ share/core.js
@@ -1,3 +1,6 @@
+String.prototype.chomp = function chomp() {
+	return this.replace(/[\s\uFEFF\xA0]+$/g, '');
+}
 
 run.prototype = {
 	toString:function(){return this.stdout || this.stderr || this.status},
