3f10b1975a4004cffc0f04cb364fb1ecb3ae9e9f
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-18 16:05:12 -0600

    Enhanced Interactive Mode Support
    Interactive Reporting on stderr is active by default.
    
    Uses the new String.prototype.chomp() JSE Feature for perl-likeness in I/O string processing.

--- share/interactive.js
+++ share/interactive.js
@@ -1,16 +1,17 @@
 
-var line = 0;
+var interactiveLineNumber = 0;
+var interactiveReporting = true;
 
 function prompt() {
 	return '[' + Environment.USER + ': ' + localPath().split("/").pop() + "] ";
 }
 
-rel: while (++line) {
+rel: while (++interactiveLineNumber) {
 	if ((script = readLine(prompt())) !== -1) {
 		while (1) {
 			script += '\n';
 			try {
-				checkSyntax(script, parameter[0], line);
+				checkSyntax(script, parameter[0], interactiveLineNumber);
 			} catch (exception) {
 				if (exception.message === "Unexpected end of script") {
 					script += readline("> ");
@@ -23,9 +24,10 @@ rel: while (++line) {
 			break;
 		}
 		try {
-			eval(script);
+			var r = eval(script);
+			if (interactiveReporting) printErrorLine(String(r).chomp());
 		} catch (exception) {
-			printErrorLine(exception);
+			printErrorLine(exception + ": " + exception.stack.toString());
 			continue rel;
 		}
 	} else exit(0);
