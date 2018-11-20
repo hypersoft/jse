551aa701ac991cc97e3a46a1cbf474759fdfc8d3
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-20 03:54:14 -0600

    Add some number-magic-utilities

--- share/core.js
+++ share/core.js
@@ -6,3 +6,9 @@ run.prototype = {
 	toString:function(){return this.stdout || this.stderr || this.status},
 	valueOf:function(){return this.status}
 };
+
+Object.defineProperty(Number.prototype, {
+	toHex:{value:function hex(number) {
+		return '0x' + Math.abs(number).toString(16);
+	}}
+});
