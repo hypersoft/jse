5fb2f221b5b27a2843df4faedf1159ec53be4e9f
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-20 11:27:36 -0600

    Adding Common File Utility

--- share/core.js
+++ share/core.js
@@ -21,3 +21,62 @@ Object.defineProperties(Number.prototype, {
 		return Math.abs(number).toString(2);
 	}},
 });
+
+var File = function File(path) {
+	if (this.constructor !== File) return new File(path);
+	this.___path___ = path;
+}
+
+Object.defineProperties(File, {
+	separator:{value: '/'},
+	pathSeparator:{value: ':'},
+	specificationSeparator:{value:'.'},
+	// readText:{},
+	// writeText:{},
+	// appendText:{},
+	// truncate:{},
+	// writeData:{},
+	// readData:{},
+	// map:{},
+	// open:{},
+	// close:{},
+	prototype:{value:Object.defineProperties({}, {
+		___path___:{value:null, enumerable:false},
+		toString:{value:function(){return ___path___;}},
+		path:{get(){return ___path___;}},
+		constructor:{value:File},
+		getChildren:{value:function(){}},
+		hasLegalName:{value:function(){/* validate name against shell operation corruption */}},
+		isEmpty:{value:function(){}},
+		isDirectory:{value:function(){}},
+		isNewerThan:{value:function(){}},
+		isOlderThan:{value:function(){}},
+		isReadable:{value:function(){}},
+		isWritable:{value:function(){}},
+		isLink:{value:function(){}},
+		isTeletype:{value:function(){/* is this pseudo file on a TTY? */}},
+		isReal:{value:function(){/* is this a real file or a pseudo file? */}},
+		isExecutable:{value:function(){}},
+		isRelative:{value:function(){return ! this.path.startsWith(File.separator);}},
+		realPath:{get(){}},
+		parent:{get(){
+			if (this.path === '/') return undefined;
+			var parent = this.path.slice(0, this.path.lastIndexOf(File.separator));
+			return new File(parent);
+		}},
+		specifications:{get(){
+			var firstDotPosition = this.path.indexOf(File.specificationSeparator);
+			if (! firstDotPosition ) return [];
+			return this.path.slice(firstDotPosition).split(File.specificationSeparator);
+		}},
+		exists:{get(){}},
+		creationTime:{get(){}},
+		lastOperationTime:{get(){}},
+		modificationTime:{get(){}},
+		owner:{get(){}},
+		group:{get(){}},
+		size:{get(){}},
+		storageSize:{get(){}},
+		storageAvailable:{get(){}},
+	})},
+});
