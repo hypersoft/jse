6b946fd0b9ca8f9aaf57626ea6378054295bfff5
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-20 08:13:54 -0600

    Add some core scripting utilities

--- share/core.js
+++ share/core.js
@@ -7,8 +7,17 @@ run.prototype = {
 	valueOf:function(){return this.status}
 };
 
-Object.defineProperty(Number.prototype, {
+Object.defineProperties(Number.prototype, {
 	toHex:{value:function hex(number) {
+		// you can parse this result with Number(VAL)
 		return '0x' + Math.abs(number).toString(16);
-	}}
+	}},
+	toOctal:{value:function hex(number) {
+		// you can parse this result with parseInt(VAL, 8)
+		return Math.abs(number).toString(8);
+	}},
+	toBinary:{value:function hex(number) {
+		// you can parse this result with parseInt(VAL, 2)
+		return Math.abs(number).toString(2);
+	}},
 });
